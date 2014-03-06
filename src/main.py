4efa194c20cdfb5bc3b746bb657eca9ed68c8343
Chris <sihrc.c.lee@gmail.com>
2014-03-06 15:10:28 -0500

    Added formatting data for training

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ Testing voiceID methods
 author: chris
 """
 from config import *
-import os, string
+import os
+import numpy as np
 import signal_processing as sp
 import features as fs
 
@@ -20,16 +21,25 @@ def getFeatures(filename):
 	return fbank_feat
 
 @debug
+def formatForTraining(data, voice):
+	"""
+	Formats data received from getFeatures for format
+	returns numpy array of data:voice
+	"""
+	target = np.ones(shape=(data.shape[0],1)) * voice
+	return np.concatenate((data,target), axis = 1)
+
+@debug
 def main():
-	voices = dict()
+	voices = []
 	for i,afile in enumerate(os.listdir(samples_path)):
-		voices[afile] = string.letters[i]
+		voices.append(i)
 		if int(afile[:-4]) <= 3000:
-			data = getFeatures(afile)
-			#TODO - addTargetFeature(data, string.letters)
+			formatForTraining(getFeatures(afile), i)
 		else:
 			#TODO - split larger audio files down to smaller ones
 			pass
+		raw_input()
 	
 
 if __name__ == "__main__":
