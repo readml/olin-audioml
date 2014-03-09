44043d402be848b99fc9d8ac86695d1898a368b1
Chris <sihrc.c.lee@gmail.com>
2014-03-09 13:36:01 -0400

    Added scoring function

--- src/formatFeatures.py
+++ src/formatFeatures.py
@@ -31,10 +31,10 @@ def getFeatures(signal, rate):
 	author: chris
 	"""
 	if signal.shape[0] > mem_cut_off:
-		mfcc = getFeatures(signal[mem_cut_off:], rate)
-		return np.concatenate((fs.mfcc(signal[:mem_cut_off],rate),mfcc))
+		mfcc,fbank = getFeatures(signal[mem_cut_off:], rate)
+		return np.concatenate((fs.mfcc(signal[:mem_cut_off],rate),mfcc)), np.concatenate((fs.logfbank(signal,rate),fbank))
 	else:
-		return fs.mfcc(signal,rate)#, fs.logfbank(signal,rate)
+		return fs.mfcc(signal,rate), fs.logfbank(signal,rate)
 
 @debug
 def formatForTraining(data, voice):
@@ -56,14 +56,17 @@ def getDataSet(files):
 	author: chris
 	"""
 	dataM = np.zeros(shape=(1,14))
+	dataF = np.zeros(shape=(1,27))
 	voices = []
 	for i,afile in enumerate(files):
-		print afile
+		if i >= 4:
+			break
 		voices.append(i)
 		rate,signal = sp.loadAudio(afile)
-		mfcc = getFeatures(signal,rate)
+		mfcc, fbank = getFeatures(signal,rate)
 		dataM = np.concatenate((dataM, formatForTraining(mfcc,i)), axis = 0) 
-	return voices, dataM
+		dataF = np.concatenate((dataF, formatForTraining(fbank, i)), axis = 0)
+	return voices, dataM, dataF
 
 @debug
 def extractFeatures(afile):
@@ -71,8 +74,8 @@ def extractFeatures(afile):
 	Extracts Feature from audio file
 	"""
 	rate,signal = sp.loadAudio(afile)
-	mfcc = getFeatures(signal,rate)
-	return mfcc
+	mfcc, fbank = getFeatures(signal,rate)
+	return mfcc, fbank
 	
 @debug
 def splitTraining(data, split= .1, onehot = True):
@@ -104,17 +107,23 @@ def one_hot(array):
 
 @debug
 def main():
-	voices,dataM = getDataSet(os.listdir(samples_path))
+	voices,dataM,dataF = getDataSet(os.listdir(samples_path))
 
 	#Getting training and testing data
 	mTrainX, mTrainY, mTestX, mTestY = splitTraining(dataM)
+	fTrainX, fTrainY, fTestX, fTestY = splitTraining(dataF)
 
 
 	# print mTrainX.shape
 	# print mTrainY.shape
 	# print mTestX.shape
 	# print mTestY.shape
-	
+
+	# print fTrainX.shape
+	# print fTrainY.shape
+	# print fTestX.shape
+	# print fTestY.shape
+
 	#MFCC
 	Mmodel = RandomForestClassifier(n_estimators = 300)
 	Mmodel.fit(mTrainX, mTrainY)
@@ -122,7 +131,18 @@ def main():
 	Mprediction =  Mmodel.predict(mTestX)
 	Maccuracy = metrics.accuracy_score(mTestY, Mprediction)
 
+
+	#FBank
+	Fmodel = RandomForestClassifier(n_estimators = 300)
+	Fmodel.fit(mTrainX, mTrainY)
+
+	Fprediction =  Fmodel.predict(mTestX)
+	Faccuracy = metrics.accuracy_score(mTestY, Fprediction)
+
+	
 	print "MFCC", Maccuracy
+	print "FBANK", Faccuracy
+	
 	
 
 if __name__ == "__main__":
--- src/gmm.py
+++ src/gmm.py
@@ -6,13 +6,41 @@ author: chris
 from config import *
 import formatFeatures as FF
 import numpy as np
+from itertools import permutations
+from operator import itemgetter
 
 from sklearn.mixture import GMM
 import os
 
-def score(predict, real):
-	return 1 #TODO
+def score(predict, real, n_components):
+	predDict = {}
+	realDict = {}
+	for val in xrange(n_components):
+		predDict[val] = set(np.where(predict == val)[0])
+		realDict[val] = set(np.where(real == val)[0])
 
+	scoreDict = {}
+	for i in xrange(n_components):
+		for j in xrange(n_components):
+			pred = predDict[i]
+			real_ = realDict[j]
+			scoreDict[(i,j)] = len(pred - real_) + len(real_ - pred)
+
+	scores = []
+	for perm in permutations(xrange(n_components)):
+		combs = zip(xrange(n_components), perm)
+		scores.append((combs,sum([scoreDict[comb] for comb in combs])))
+	scores.sort(key = itemgetter(1))
+	mapping = dict(scores[0][0])
+	print mapping
+	print predict
+	print scores[0]
+	mapped_predict = [mapping[val] for val in predict]
+	return float(len(np.where(mapped_predict - real == 0)[0]))/predict.shape[0]
+	
+			
+
+	return 1
 if __name__ == "__main__":
 	voices,dataM,dataF = FF.getDataSet(os.listdir(samples_path))
 
@@ -20,12 +48,16 @@ if __name__ == "__main__":
 	mTrainX, mTrainY, mTestX, mTestY = FF.splitTraining(dataM, onehot = False)     
 	fTrainX, fTrainY, fTestX, fTestY = FF.splitTraining(dataF, onehot = False)
 
-	model = GMM()
-	model.fit(fTrainX)
+	model = GMM(len(voices))
+	model.fit(mTrainX)
 
-	predict = model.predict_proba(fTestX)
-	print predict
-	score(predict, fTestY)
+	predict = model.predict(mTestX)
+#	print score(predict, mTestY, len(voices))
+
+	# a = np.array([0,1,1,2])
+	# b = np.array([0,1,2,1])
+
+	# print score(a,b,3)
 
 
 
